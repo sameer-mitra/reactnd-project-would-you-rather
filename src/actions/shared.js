c129b5b925eaf67751e7bcc75bb475844311dead
sameer-mitra <49610563+sameer-mitra@users.noreply.github.com>
2020-08-06 20:12:14 +1000

    SM 20200806 Added Middleware and logger.js.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,7 +3,7 @@ import { receiveQuestions } from '../actions/questions';
 import { receiveUsers } from '../actions/users';
 
 export function handleInitialData() {
-  return dispatch => {
+  return (dispatch) => {
     return getInitialData().then(({ users, questions }) => {
       dispatch(receiveQuestions(questions));
       dispatch(receiveUsers(users));
